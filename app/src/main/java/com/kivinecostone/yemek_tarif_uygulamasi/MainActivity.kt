5819637a07bf727fd8bd1c936a7d111c4930c888
kivancsakuc <kivancsakuc@gmail.com>
2025-08-06 00:55:27 +0300

    Fragmentlar ve Activityler Oluşturuldu. Uygulamanın temeli atıldı. Rahatlıkla başlayabiliriz.

--- app/src/main/java/com/kivinecostone/yemek_tarif_uygulamasi/MainActivity.kt
+++ app/src/main/java/com/kivinecostone/yemek_tarif_uygulamasi/MainActivity.kt
@@ -5,16 +5,48 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val homeFragment = HomeFragment()
+    private val cameraFragment = CameraFragment()
+    private val calendarFragment = CalendarFragment()
+    private val savedFragment = SavedFragment()
+    private val aiChatFragment = AiChatFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        replaceFragment(homeFragment)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> replaceFragment(homeFragment)
+                R.id.nav_camera -> replaceFragment(cameraFragment)
+                R.id.nav_calendar -> replaceFragment(calendarFragment)
+                R.id.nav_saved -> replaceFragment(savedFragment)
+                R.id.nav_ai_chat -> replaceFragment(aiChatFragment)
+            }
+            true
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
