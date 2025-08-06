4200762e0601c26b3cb5b30bd0ca19e71192d2a1
OrhanUnal <140164675+OrhanUnal@users.noreply.github.com>
2025-08-06 16:08:30 +0300

    Kamera ile fotograf cekme yapildi

--- app/src/main/java/com/kivinecostone/yemek_tarif_uygulamasi/CameraFragment.kt
+++ app/src/main/java/com/kivinecostone/yemek_tarif_uygulamasi/CameraFragment.kt
@@ -1,13 +1,87 @@
 package com.kivinecostone.yemek_tarif_uygulamasi
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+
 
 class CameraFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    companion object{
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val CAMERA_REQUEST_CODE = 2
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btn_camera = view.findViewById<Button>(R.id.btn_camera)
+        btn_camera.setOnClickListener{
+            if (ContextCompat.checkSelfPermission(requireContext(),
+                    Manifest.permission.CAMERA
+                )== PackageManager.PERMISSION_GRANTED
+            ){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }
+            else{
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_PERMISSION_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }else{
+                Toast.makeText(
+                    requireContext(),
+                    "Oops, you just denied the permission for camera. "+
+                            "Please allow it in the settings.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK){
+            if (requestCode == CAMERA_REQUEST_CODE){
+                val thumBnail: Bitmap = data!!.extras!!.get("data") as Bitmap
+                val iv_image = view?.findViewById<ImageView>(R.id.iv_image)
+                iv_image?.setImageBitmap(thumBnail)
+            }
+        }
     }
 }
--- app/src/main/java/com/kivinecostone/yemek_tarif_uygulamasi/MainActivity.kt
+++ app/src/main/java/com/kivinecostone/yemek_tarif_uygulamasi/MainActivity.kt
@@ -1,8 +1,19 @@
 package com.kivinecostone.yemek_tarif_uygulamasi
 
+import android.Manifest
+import android.app.Activity
+import android.app.ComponentCaller
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
