f79b98405980f50f364f7b00aacc5bc85b3eea34
OrhanUnal <140164675+OrhanUnal@users.noreply.github.com>
2025-08-16 23:33:08 +0300

    Add theme switcher and update icons for dark mode
    Introduced a theme switch (SwitchCompat) in MainActivity with persistent night mode using SharedPreferences. Updated drawable resources: replaced vector icons with PNGs for calendar, camera, chat, home, and saved; added new drawables for theme switch thumb and track. Modified layouts and styles to support light/dark themes, including new color definitions and style resources for backgrounds and tints. Updated bottom navigation menu to use new icons.

--- app/src/main/java/com/kivinecostone/yemek_tarif_uygulamasi/MainActivity.kt
+++ app/src/main/java/com/kivinecostone/yemek_tarif_uygulamasi/MainActivity.kt
@@ -3,21 +3,27 @@ package com.kivinecostone.yemek_tarif_uygulamasi
 import android.Manifest
 import android.app.Activity
 import android.app.ComponentCaller
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.provider.MediaStore
+import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,6 +37,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val switchMode: SwitchCompat = findViewById(R.id.themeSwitch)
+        val sharedPreferences: SharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE)
+        val nightMode: Boolean = sharedPreferences.getBoolean("nightMode", false)
+        if(nightMode){
+            switchMode.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+        switchMode.setOnCheckedChangeListener { _, isChecked ->
+            if (nightMode) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                sharedPreferences.edit {
+                    putBoolean("nightMode", false)
+                }
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                sharedPreferences.edit {
+                    putBoolean("nightMode", true)
+                }
+            }
+        }
 
         replaceFragment(homeFragment)
 
