536eb8061a2a3c81fcc08b13300775766f09cb0f
kivancsakuc <kivancsakuc@gmail.com>
2025-09-04 20:19:53 +0300

    Remove unused imports from MainActivity.kt
    Cleaned up MainActivity.kt by deleting unnecessary import statements to improve code readability and maintainability.

--- app/src/main/java/com/kivinecostone/yemek_tarif_uygulamasi/MainActivity.kt
+++ app/src/main/java/com/kivinecostone/yemek_tarif_uygulamasi/MainActivity.kt
@@ -1,24 +1,13 @@
 package com.kivinecostone.yemek_tarif_uygulamasi
 
-import android.Manifest
-import android.app.Activity
-import android.app.ComponentCaller
+
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.provider.MediaStore
-import android.view.View
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SwitchCompat
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
